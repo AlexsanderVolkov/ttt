ddfab66504ffbf51a47f8dbe4d62e33e1c0226dc
AlexsanderVolkov <wav74@inbox.ru>
2020-02-27 09:42:42 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {PersistentDrawerLeft} from './demo';
+
+ReactDOM.render(<PersistentDrawerLeft />, document.querySelector('#root'));
+    
